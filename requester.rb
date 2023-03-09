05e7351cc64b71fb56a54a68fb301a01bef5f0aa
Jorge Rosano <jorg.ross.13@gmail.com>
2023-03-09 15:13:24 -0600

    add welcome menu

--- requester.rb
+++ requester.rb
@@ -1,6 +1,8 @@
 module Requester
   def select_main_menu_action
     # prompt the user for the "random | scores | exit" actions
+    options = %w[random scores exit]
+    gets_option(options)
   end
 
   def ask_question(question)
@@ -17,8 +19,18 @@ module Requester
     # prompt the user to give the score a name if there is no name given, set it as Anonymous
   end
 
-  def gets_option(prompt, options)
+  def gets_option(options)
     # prompt for an input
+    puts options.join (" | ")
+    print "> "
+    input = gets.chomp
+    
+    until options.include?(input)
+      puts "Invalid option"
+      print "> "
+      input = gets.chomp
+    end
+
     # keep going until the user gives a valid option
   end
 end
